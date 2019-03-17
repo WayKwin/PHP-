d652a06b0dcf5a0e2e96d63fa8ef4405f3fa7e52
WayKwin <543235396@qq.com>
2019-03-17 10:25:34 +0800

    start WeAdmin

--- Frame/Vendor/Smarty.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Frame\Vendor;
-// smarty类 加上命名空间
-require_once(FRAME_PATH."Vendor".DS."Smarty".DS."libs".DS."Smarty.class.php");
- final class Smarty extends \Smarty
-{
-    //作用仅仅加上命名空间,继承原始Smary类即可
-}
- ?>
